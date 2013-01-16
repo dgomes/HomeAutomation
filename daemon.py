0fb64e4dcec78a067ef18d4ffd4575634094ae53
Diogo Gomes <dgomes@ua.pt>
2013-01-16 00:43:19 +0000

    added webservices to control IR and RF

--- daemon.py
+++ daemon.py
@@ -3,22 +3,25 @@ import httplib
 import yaml
 import sys
 import urllib2
+from os import path
 
-from twisted.internet import reactor 
+from serial.serialutil import SerialException
+
+from twisted.internet import reactor
 from twisted.protocols.basic import LineOnlyReceiver
-from twisted.internet.serialport import SerialPort 
+from twisted.internet.serialport import SerialPort
 from twisted.web import server, resource
 from twisted.python import log
 
-from twisted.words.xish import domish
-from twisted.words.protocols.jabber.jid import JID
-from wokkel.client import XMPPClient
-from wokkel.xmppim import RosterClientProtocol, Message
-
 import json
 
+import sony
+import chacon
+
+serial = False
 conf = yaml.load(file('settings.yaml', 'r'))
-log.startLogging(sys.stdout) 
+log.startLogging(sys.stdout)
+#log.startLogging(open(conf['logfile'], 'w'))
 
 class USBClient(LineOnlyReceiver):
 
@@ -42,11 +45,44 @@ class USBClient(LineOnlyReceiver):
 		print "outReceived! with %d bytes!" % len(data)
 		self.data = self.data + data
 
-class CommandResource(resource.Resource):
-    isLeaf = True    
-    def render_GET(self, request):
-	print request
-        return "I am a request \n"
+class InfoResource(resource.Resource):
+	def getChild(self, name, request):
+		print "Name: ", name
+		print "Request: ", request
+		return self
+
+	def render_GET(self, request):
+		return "<html>Info</html>"
+
+class IRCommandResource(resource.Resource):
+	isLeaf = True
+	def render_GET(self, request):
+		command = request.path.split('/')
+		print command
+		try:
+			cmd = sony.codes[command[2]]
+			serial.writeSomeData(cmd)
+		except:
+			log.err()
+			return "<html>IR Failed</html>"
+		return "<html>IR Command!</html>"
+
+class RFCommandResource(resource.Resource):
+	isLeaf = True
+	def render_GET(self, request):
+		command = request.path.split('/')
+		try:
+			cmd = chacon.codes[command[2]][command[3]]
+			serial.writeSomeData(cmd)
+		except:
+			log.err()
+			return "<html>RF Failed</html>"
+		return "<html>RF Sent!</html>"
+
+class WeatherCommandResource(resource.Resource):
+	isLeaf = True
+	def render_GET(self, request):
+		return "<html>Weather Station</html>"
 
 
 def updateCOSM(line):
@@ -55,7 +91,7 @@ def updateCOSM(line):
 	except:
 		return
 
-	if d[u'code'] != 100:
+	if d[u'code'] != 100:  #we only care for weather station reports (code 100)
 		return
 	datastream = json.dumps({"version": "1.0.0", "datastreams": [ { "id": "humidity", "current_value": d[u'Humidity'] }, {"id": "temperature", "current_value": d[u'Temperature'] }]})
 	headers = {"Content-type": "text/json", "X-ApiKey": conf['cosm']['ApiKey']}
@@ -66,6 +102,13 @@ def updateCOSM(line):
 	print "COSM update"
 
 if __name__ == "__main__":
-	reactor.listenTCP(conf['port'], server.Site(CommandResource()))
-	SerialPort(USBClient(updateCOSM), conf['serial']['port'], reactor, baudrate=conf['serial']['baudrate'])
+	root = InfoResource()
+	root.putChild("IR", IRCommandResource())
+	root.putChild("RF", RFCommandResource())
+	root.putChild("Weather", WeatherCommandResource())
+	reactor.listenTCP(conf['port'], server.Site(root))
+	try:
+		serial = SerialPort(USBClient(updateCOSM), conf['serial']['port'], reactor, baudrate=conf['serial']['baudrate'])
+	except SerialException as e:
+		log.err()
 	reactor.run()
