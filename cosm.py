d5cf8ca2bcc50622a3a942c805e2874208d54083
Diogo Gomes <dgomes@ua.pt>
2013-02-03 00:47:27 +0000

    per update feed id

--- cosm.py
+++ cosm.py
@@ -3,20 +3,22 @@ from txcosm.HTTPClient import HTTPClient
 
 class CosmInterface():
 	def __init__(self, conf):
-		self.cosmClient = HTTPClient(api_key=conf['cosm']['ApiKey'], feed_id=conf['cosm']['feed'])
+		self.conf = conf
+
 	def successlog(self, msg):
 		print "Successfully published to COSM"
 
 	def errorlog(self, msg):
 		print "Error publishing: ", msg
 
-	def updateCOSM(self, data):
+	def updateCOSM(self, data, feed):
+		cosmClient = HTTPClient(api_key=self.conf['cosm']['ApiKey'], feed_id=feed)
 		environment = txcosm.Environment(version="1.0.0")
 		for i in data.keys():
 			environment.setCurrentValue(i, data[i])
 
 		# Update the Cosm service with latest value(s)
-		d = self.cosmClient.update_feed(data=environment.encode())
+		d = cosmClient.update_feed(data=environment.encode())
 		d.addCallback(self.successlog)
 		d.addErrback(self.errorlog)
 
