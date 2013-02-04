84db0fe4c99f10cdd99442063a344bd7d8dd1f13
Diogo Gomes <dgomes@ua.pt>
2013-02-04 23:54:10 +0000

    no need anymore
    now part of home.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def jsonpCallback(request, data):
-	callback = request.args.get('callback')
-
-	if callback:
-		callback = callback[0]
-		data = '%s(%s);' % (callback, data)
-		return data
-	return "callback({'error': 'no callback defined'})"
-
