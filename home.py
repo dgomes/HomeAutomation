264daee1f3702aaf87689e56a0418012a9466eb9
Diogo Gomes <dgomes@ua.pt>
2013-02-04 23:33:23 +0000

    Parent Resource Class for all Resource's that are periodicaly called

---
+++ home.py
@@ -0,0 +1,30 @@
+from twisted.internet import task
+from twisted.web import resource
+from twisted.internet import reactor
+
+class Resource(resource.Resource):
+	isLeaf = True
+	def __init__(self, sink, conf):
+		if not hasattr(self, 'data'):
+			self.data = None
+		self.dataSink = sink
+		self.conf = conf
+		l = task.LoopingCall(self.cronJob)
+		l.start(float(conf[self.confid]['pool_interval']))
+
+	def _jsonpCallback(request, data):
+		callback = request.args.get('callback')
+
+		if callback:
+			callback = callback[0]
+			data = '%s(%s);' % (callback, data)
+			return data
+		return "callback({'error': 'no callback defined'})"
+
+	def render_GET(self, request):
+		data = json.dumps(self.data)
+		return self._jsonpCallback(request, data)
+
+	def cronJob(self):
+		raise NotImplementedError("Subclasses should implement this!")
+
